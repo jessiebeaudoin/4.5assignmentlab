63691987031ba3fdf6c78e24418d75f150269d9a
jessiebeaudoin <123115086+jessiebeaudoin@users.noreply.github.com>
2025-02-08 20:37:15 +0000

    adding scroll

---
+++ scripts/scrollreveal.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    ScrollReveal().reveal('.card', {
+      duration: 1000,
+      scale: 0.8,
+      distance: '20px',
+    });
+  });
+  
