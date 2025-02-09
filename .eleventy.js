577953952acc38445e8ab87904d0c8c2b43289c8
jessiebeaudoin <123115086+jessiebeaudoin@users.noreply.github.com>
2025-02-09 00:24:37 +0000

    updating configuration

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,11 @@
 module.exports = function(eleventyConfig) {
   return {
-    templateFormats: ["liquid", "md", "html"],
+    templateFormats: ["liquid", "md", "html"], // Specify the template formats you're using
     dir: {
-      input: ".",
-      includes: "_includes",
-      data: "_data",
-      output: "_site"
+      input: ".",          // Input directory
+      includes: "_includes", // Includes directory
+      data: "_data",        // Data directory
+      output: "_site"       // Output directory
     }
   };
 };
